e2693835b66160a54511ff19448d206b44215de3
Marc Mila <milamartosmarc5@gmail.com>
2021-03-29 09:40:14 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# setupVehicle
+# setupVehicle
---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt-get install python -y
+sudo apt-get install python-pip -y
+sudo apt-get install python-setuptools -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-setuptools -y
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+sudo apt-get install git -y
+sudo apt-get install sqlite3 -y
+
+
+sudo pip install pick
+sudo pip install psutil
+sudo pip install socketIO_client_nexus
+
+sudo pip3 install pick
+sudo pip3 install psutil
+sudo pip3 install cherrypy
+sudo pip3 install hug==2.4.1
+sudo pip3 install pymongo
+sudo pip3 install hug_middleware_cors
+sudo pip3 install socketIO_client_nexus
+
+sudo mkdir /etc/agent
+sudo mkdir /etc/agent/codes
+sudo mkdir /etc/agent/config
+sudo chmod -R 777 /etc/agent
+
+wget https://raw.githubusercontent.com/marc-mila/environmentVehicle/main/insert_sqlite.py
+wget -O map.db https://github.com/marc-mila/environmentVehicle/blob/main/map.db?raw=true
+
+mv insert_sqlite.py /etc/agent/
+mv map.db /etc/agent/
+
+git clone https://github.com/marc-mila/codesVehicle.git
+mv /etc/agent/codesVehicle/codes_def.tar /etc/agent/codes/
+tar xvf codes_def.tar
+
+echo "Test"
+hug -f /etc/agent/codes/api.py
+
+
