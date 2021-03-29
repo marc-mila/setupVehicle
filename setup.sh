61a123916a27ef0c197d28ff2a04ee4b486591fb
Marc Mila <milamartosmarc5@gmail.com>
2021-03-29 10:00:17 +0000

    paths fixed

--- setup.sh
+++ setup.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
+#sudo apt-get update -y
+#sudo apt-get upgrade -y
 
-sudo apt-get install python -y
-sudo apt-get install python-pip -y
-sudo apt-get install python-setuptools -y
-sudo apt-get install python3 -y
-sudo apt-get install python3-pip -y
-sudo apt-get install python3-setuptools -y
-sudo apt-get install nodejs -y
-sudo apt-get install npm -y
-sudo apt-get install git -y
-sudo apt-get install sqlite3 -y
+#sudo apt-get install python -y
+#sudo apt-get install python-pip -y
+#sudo apt-get install python-setuptools -y
+#sudo apt-get install python3 -y
+#sudo apt-get install python3-pip -y
+#sudo apt-get install python3-setuptools -y
+#sudo apt-get install nodejs -y
+#sudo apt-get install npm -y
+#sudo apt-get install git -y
+#sudo apt-get install sqlite3 -y
 
 
-sudo pip install pick
-sudo pip install psutil
-sudo pip install socketIO_client_nexus
+#sudo pip install pick
+#sudo pip install psutil
+#sudo pip install socketIO_client_nexus
 
-sudo pip3 install pick
-sudo pip3 install psutil
-sudo pip3 install cherrypy
-sudo pip3 install hug==2.4.1
-sudo pip3 install pymongo
-sudo pip3 install hug_middleware_cors
-sudo pip3 install socketIO_client_nexus
+#sudo pip3 install pick
+#sudo pip3 install psutil
+#sudo pip3 install cherrypy
+#sudo pip3 install hug==2.4.1
+#sudo pip3 install pymongo
+#sudo pip3 install hug_middleware_cors
+#sudo pip3 install socketIO_client_nexus
 
 sudo mkdir /etc/agent
 sudo mkdir /etc/agent/codes
@@ -41,7 +41,7 @@ mv map.db /etc/agent/
 
 git clone https://github.com/marc-mila/codesVehicle.git
 mv /etc/agent/codesVehicle/codes_def.tar /etc/agent/codes/
-tar xvf codes_def.tar
+tar xvf /etc/agent/codes/codes_def.tar
 
 echo "Test"
 hug -f /etc/agent/codes/api.py
